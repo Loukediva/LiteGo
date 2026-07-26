3ddfff359ede6188261a859792dff985d517b4bd
Loukediva <113971198+Loukediva@users.noreply.github.com>
2026-07-26 17:18:09 +0000

    Add files via upload

---
+++ LiteGo/app/src/main/java/com/litego/browser/AdBlocker.kt
@@ -0,0 +1,17 @@
+
+package com.litego.browser
+
+object AdBlocker {
+    // Liste ultra légère - ajoute ici les domaines de pub
+    private val blockedHosts = setOf(
+        "doubleclick.net",
+        "googlesyndication.com",
+        "googleadservices.com",
+        "adservice.google",
+        "facebook.com/tr",
+        "ads-twitter.com"
+    )
+    fun isAd(url: String): Boolean {
+        return blockedHosts.any { url.contains(it) }
+    }
+}
