4ad30506c13b8ec715c89b65b2cccd488a5a000a
Loukediva <113971198+Loukediva@users.noreply.github.com>
2026-07-26 19:08:54 +0000

    Delete app directory

--- app/src/main/java/com/litego/browser/AdBlocker.kt
+++
@@ -1,5 +0,0 @@
-package com.litego.browser
-object AdBlocker {
-    private val blockedHosts = setOf("doubleclick.net","googlesyndication.com","googleadservices.com","adservice.google","facebook.com/tr","ads-twitter.com")
-    fun isAd(url: String): Boolean { return blockedHosts.any { url.contains(it) } }
-}
